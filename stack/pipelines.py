d3084ed4ea45bd800d7080b9d79e8dc5f0a8a32d
chengyangliu47 <1996chengyangliiu@gmail.com>
2020-08-09 22:38:36 -0400

    First Spider

---
+++ stack/pipelines.py
@@ -0,0 +1,41 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+import pymongo
+from scrapy import Item
+from scrapy import settings
+from scrapy.exceptions import DropItem
+
+
+class StackPipeline:
+    def process_item(self, item, spider):
+        return item
+
+
+class MongoDBPipeline(object):
+    collection_name = 'scrapy_items'
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        cls.DB_URL = crawler.settings.get('MONGO_DB_URI', 'mongodb://localhost:27017')
+        cls.DB_NAME = crawler.settings.get('MONGO_DB_NAME', 'scrapy_data')
+        return cls()
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.DB_URL)
+        self.db = self.client[self.DB_NAME]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        collection = self.db[spider.name]
+        post = dict(item) if isinstance(item, Item) else item
+        collection.insert_one(post)
+        return item
